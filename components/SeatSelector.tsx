a0bfcb5228e614bafa7bd9cad960d70a095cffb0
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-11-20 23:46:14 +0100

    created seatselector.ts

---
+++ components/SeatSelector.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState } from 'react';
+
+export default function SeatSelector() {
+    const [selectedSeat, setSelectedSeat] = useState<number | null>(null);
+  return (
+    <div>
+    <h2> </h2>
+    </div>
+  )
+}
