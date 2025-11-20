6fb6448bcd283589f0b9b17ea658c35b63eda3b2
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-11-20 23:34:22 +0100

    adding folders and mockdata for building UI-components

---
+++ lib/mockData.ts
@@ -0,0 +1,10 @@
+export const movies = [
+    {
+        id: "1",
+        title: "Example Movie",
+        description: "A great movie!",
+        runtime: 120,
+        posterUrl: "/example.jpg",
+        genres: ["Action"],
+    },
+];
